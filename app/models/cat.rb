2ecc156a72dffa8ed04492330ecbf75a7d91f72f
MasatoYamanaka <106641837+mhsdrt@users.noreply.github.com>
2023-02-15 12:47:19 +0000

    エラー修正

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -9,7 +9,7 @@ class Cat < ApplicationRecord
   def get_cat_image(width, height)
   unless cat_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    cat_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
   cat_image.variant(resize_to_limit: [width, height]).processed
   end
