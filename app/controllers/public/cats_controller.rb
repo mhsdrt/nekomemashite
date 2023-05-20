289cd2fef1b567019ae67fb912c095542bcf53b7
MasatoYamanaka <106641837+mhsdrt@users.noreply.github.com>
2023-05-20 14:57:10 +0000

    ネコ編集画面作成中

--- app/controllers/public/cats_controller.rb
+++ app/controllers/public/cats_controller.rb
@@ -13,6 +13,20 @@ class Public::CatsController < ApplicationController
     end
   end
   
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+  
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      flash[:notice] = "You have updated cat successfully."
+    redirect_to cat_path
+    else
+      render :edit
+    end
+  end
+  
   def index
     @member = current_member
     @cats = @member.cats
